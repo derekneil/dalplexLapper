af39adaaf7e5b4e7d75a01a02774111ecf2356e9
Derek Neil <derekrneil@gmail.com>
2013-11-05 20:32:50 -0400

    trying to get everything wired to one controller

--- dalplexlaper/ViewController.m
+++ dalplexlaper/ViewController.m
@@ -13,6 +13,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
 - (IBAction)tapAction:(id)sender;
 - (IBAction)longPressAction:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *lapDistance;
+@property (weak, nonatomic) IBOutlet UISwitch *announcePace;
+- (IBAction)swipeAction:(id)sender;
 
 @end
 
@@ -22,6 +25,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,8 +35,19 @@
 }
 
 - (IBAction)tapAction:(id)sender {
+    NSLog(@"tap action");
 }
 
 - (IBAction)longPressAction:(id)sender {
+    NSLog(@"long press action");
+}
+- (IBAction)swipeAction:(id)sender {
+    NSLog(@"swipeAction");
+//    if (sender == UISwipeGestureRecognizerDirectionLeft) {
+//        NSLog(@"Left");
+//    }
+//    if(sender == UISwipeGestureRecognizerDirectionRight) {
+//        NSLog(@"Right");
+//    }
 }
 @end
