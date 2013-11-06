9574762f81413dbe6f5fa8bb79242e33960c71e8
Derek Neil <derekrneil@gmail.com>
2013-11-05 21:55:55 -0400

    remove useless crap

--- dalplexlaper/ViewController.m
+++ dalplexlaper/ViewController.m
@@ -9,21 +9,20 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+
 @property (weak, nonatomic) IBOutlet UILabel *lapsLabel;
 @property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
 - (IBAction)tapAction:(id)sender;
-- (IBAction)longPressAction:(id)sender;
 - (IBAction)swipeToSettingsAction:(id)sender;
+
 @property (weak, nonatomic) IBOutlet UITextField *lapDistance;
 @property (weak, nonatomic) IBOutlet UISwitch *announcePace;
 - (IBAction)swipeAction:(id)sender;
+
 @property (nonatomic, assign) int laps;
 @property (nonatomic, assign) float lapDistanceKM;
 @property (nonatomic, assign) float totalDistanceKM;
 @property (nonatomic, assign) BOOL announce;
-
-@property (strong, nonatomic) UIView* laptapView;
-@property (strong, nonatomic) UIView* settingsView;
 
 @end
 
@@ -38,12 +37,6 @@
     //set base values for locals
     _laps = 0;
     _lapDistanceKM = 0.26;
-    
-    //default view already created for me
-//    _laptapView = self.view;
-    
-    //create ref to settings view
-    //_settingsView = [[NSBundle mainBundle] loadNibNamed:@"settings" owner:self options:nil];
 }
 
 - (IBAction)tapAction:(id)sender {
@@ -55,21 +48,15 @@
     _distanceLabel.text = [NSString stringWithFormat:@"%.2f",_totalDistanceKM];
 }
 
-- (IBAction)longPressAction:(id)sender {
-    NSLog(@"long press action");
-    //go to settings view
-//    self.view = _settingsView;
-}
-
 - (IBAction)swipeToSettingsAction:(id)sender {
     NSLog(@"swipe to settings action");
     //go to settings view
-
 }
 
 - (IBAction)swipeAction:(id)sender {
     NSLog(@"swipeAction");
     //go back to main view
-//    self.view = _laptapView;
 }
+
+
 @end
